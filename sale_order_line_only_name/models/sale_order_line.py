db6536da6c784e0447363f927269b5c5beaad165
nahe-consulting-group <66278632+nahe-consulting-group@users.noreply.github.com>
2020-08-25 20:04:25 -0300

    Add files via upload

---
+++ sale_order_line_only_name/models/sale_order_line.py
@@ -0,0 +1,22 @@
+# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res = super(SaleOrderLine, self).product_id_change()
+        if not self.product_id:  # pragma: no cover
+            return res
+        if (self.user_has_groups(
+                'sale_order_line_description.'
+                'group_use_product_description_per_so_line') and
+                self.product_id.description_sale):
+            #self.name = self.product_id.description_sale  ESTA ERA LA ORIGINAL pero queremos que las ordenes de venta solo muestren el nombre del producto
+            self.name = self.product_id.name
+        return res
